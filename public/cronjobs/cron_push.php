6dc0e7a84a94a4b14b4b0750ca79b04ccefac567
Elias <elias@commandg.de>
2022-12-13 14:31:12 +0100

    hz

--- public/cronjobs/cron_push.php
+++
@@ -1,6 +0,0 @@
-<?php
-exec("/bin/bash /www/htdocs/w01abbc7/command-g.de/public/cronjobs/pushing.sh 2>&1", $out, $result);
-echo "Returncode: " .$result ."<br>";
-echo "Ausgabe des Scripts: " ."<br>";
-echo "<pre>"; print_r($out);
-?>
