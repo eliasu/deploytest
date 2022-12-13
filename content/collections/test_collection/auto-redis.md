6acea0b58a472fb0dc1089af2cee081d807ddb19
eliasu <elias@commandg.de>
2022-12-13 13:29:56 +0100

    Updated Tue Dec 13 13:29:56 CET 2022

---
+++ content/collections/test_collection/auto-redis.md
@@ -0,0 +1,7 @@
+---
+id: 21e367f5-4c9c-4eee-b370-d4645323db05
+blueprint: test_collection
+title: auto
+updated_by: f5fe1958-774a-4886-a03e-0b74ece8600a
+updated_at: 1670928625
+---
--- public/cronjobs/pushing.sh
+++ public/cronjobs/pushing.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 echo "Git Push started ..."
-git push
+cd /www/htdocs/w01abbc7/command-g.de
+git config user.email "elias@commandg.de"
+git config user.name "eliasu"
+
+git add -A
+git commit -a -m "Updated $(date)"
+git push origin main
+
 echo "Git Push completed!"
